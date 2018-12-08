b75f1422bea69a37cbb5a37c6bec086446fe75d3
Burt Zheng <504779282@qq.com>
2018-12-08 21:19:36 +0800

    导航栏flex布局

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,7 +1,24 @@
 //index.js
 //获取应用实例
+const catalogrMap = {
+  'gn': '国内',
+  'gj': '国际',
+  'cj': '财经',
+  'yl': '娱乐',
+  'js': '军事',
+  'ty': '体育',
+  'other': '其他',
+}
 
 
 Page({
+  data: {
+    catalogTitle: ["国内", "国际", "财经", "娱乐", "军事", "体育", "其他"],
+    
+  },
 
+
+  onLoad() {
+    
+  }
 })
